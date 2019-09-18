5248aba114abb8a2a10b3a8f2418e9ef325858b5
nldreyer <nldreyer@ksu.edu>
2019-09-18 15:22:14 -0500

    Worked on Side and Fryceritops.

--- Menu/Sides/Fryceritops.cs
+++ Menu/Sides/Fryceritops.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using DinoDiner.Menu.Sides;
 
-namespace Menu.Sides
+namespace DinoDiner.Menu.Sides
 {
-    class Fryceritops : Side
+    public class Fryceritops : Side
     {
+
+        private Size size;
+
+        public Fryceritops()
+        {
+            this.Price = 0.99;
+            this.Calories = 222;
+            this.Ingredients = new List<string>() { "Potato", "Salt", "Vegetable Oil" };
+        }
+
+        public override Size Size
+        {
+            get { return size; }
+            set
+            {
+                size = value;
+                switch (size)
+                {
+                    case Size.Small:
+                        this.Price = 0.99;
+                        this.Calories = 222;
+                        break;
+                    case Size.Medium:
+                        this.Price = 1.45;
+                        this.Calories = 365;
+                        break;
+                    case Size.Large:
+                        this.Price = 1.95;
+                        this.Calories = 480;
+                        break;
+                }
+            }
+        }
     }
 }
